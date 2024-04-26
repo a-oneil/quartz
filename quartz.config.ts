d4eb48cc7a8ba06fbba6262ff648e9256be6ee09
Austin ONeil <austino5308@gmail.com>
2024-04-26 01:08:23 -0400

    update to v4

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,14 +8,14 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "ONeils Lab",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "austinoneil.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
--- sync.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-npx quartz sync
